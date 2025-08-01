29503410c0d1332cebd0504fb969bb395c12753a
Clara Ferraz <claraferraz@proton.me>
2025-08-01 16:28:16 -0300

    Feat: not found page

---
+++ src/components/NotFoundMessage/NotFoundMessage.tsx
@@ -0,0 +1,15 @@
+export const NotFoundMessage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mt-33 ">
+      <h1>😕</h1>
+      <h3 className="font-bold dark:text-white mt-11 mb-6">
+        No Definitions Found
+      </h3>
+      <p className="text-light-gray-1 text-center">
+        Sorry pal, we couldn't find definitions for the word you were looking
+        for. You can try the search again at later time or head to the web
+        instead.
+      </p>
+    </div>
+  );
+};
