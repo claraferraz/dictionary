33ddc6a48b81807fa73a57aa519cb9a623b95e18
Clara Ferraz <claraferraz@proton.me>
2025-08-04 20:44:38 -0300

    Feat: using the word hook

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,43 +1,14 @@
 import { ResultContent } from "../components/ResultContent/ResultContent";
-import type {
-  DictionaryEntry,
-  Dictionary,
-} from "../service/DictionaryEntryType";
+import { useWord } from "../context/WordContext";
 
-type Props = {
-  result: DictionaryEntry[];
-};
-
-export const ResultPage = ({ result }: Props) => {
-  const filteredResult: Dictionary[] = result.map((r) => {
-    const phonetic = r.phonetics.find((phonetic) => phonetic.audio != "");
-    const meanings = r.meanings.map((m) => {
-      const definitions = m.definitions.map((d) => {
-        return {
-          definition: d.definition,
-          example: d.example || undefined,
-        };
-      });
-      return {
-        partOfSpeech: m.partOfSpeech,
-        definitions: definitions,
-        synonyms: m.synonyms,
-      };
-    });
-    return {
-      word: r.word,
-      phonetic: r.phonetic || undefined,
-      audio: phonetic?.audio || undefined,
-      meanings: meanings,
-      sourceUrls: r.sourceUrls,
-    };
-  });
+export const ResultPage = () => {
+  const { definition } = useWord();
 
   return (
     <div className="pb-28">
-      {filteredResult &&
-        filteredResult.map((r) => {
-          return <ResultContent dictionary={r} />;
+      {definition &&
+        definition.map((d, i) => {
+          return <ResultContent dictionary={d} index={i} />;
         })}
     </div>
   );
