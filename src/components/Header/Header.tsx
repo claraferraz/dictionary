aa5d0186041be878272f2c420be79011bd48f53c
Clara Ferraz <claraferraz@proton.me>
2025-08-05 18:20:31 -0300

    Styles: header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,14 @@ import { FontDropout } from "../FontDropout/FrontDropout";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center w-full h-9">
+    <div className="flex justify-between items-center w-full h-8 sm:h-9">
       <img src="src/assets/images/logo.svg" alt="book doodle" />
       <div className="flex items-center">
         <div>
           <FontDropout />
         </div>
-        <div className="flex w-27 ml-6.5 items-center justify-between">
-          <div className="w-0.5 h-8 bg-light-gray-2"></div>
+        <div className="flex w-22 gap-4 ml-4 sm:w-27 sm:gap-6.5 sm:ml-6.5 items-center">
+          <div className="w-[0.0625rem] h-8 bg-light-gray-2" />
           <DarkToggle />
         </div>
       </div>
