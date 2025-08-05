de79a83d4fb1d46c3b2f9bd29ac6c6edf635a905
Clara Ferraz <claraferraz@proton.me>
2025-08-05 17:14:28 -0300

    Styles: responsive layout

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -5,9 +5,11 @@ export const Body = ({ children }: PropsWithChildren) => {
   const { theme, font } = useTheme();
   return (
     <div
-      className={`${theme} ${font} dark:bg-dark-gray-1 h-full min-h-[100dvh] w-full  flex justify-center transition-colors`}
+      className={`${theme} ${font} dark:bg-dark-gray-1 h-full min-h-[100dvh] w-full flex justify-center transition-colors`}
     >
-      <div className="w-3xl mt-17">{children}</div>
+      <div className="w-94 m-6 md:w-3xl sm:mx-10 sm:mt-17 md:mt-14">
+        {children}
+      </div>
     </div>
   );
 };
