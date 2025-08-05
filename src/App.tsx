01f1a21f7dd1c664cc133d5f9f2f19bdee6d46c2
Clara Ferraz <claraferraz@proton.me>
2025-08-05 16:05:06 -0300

    Fix: body, header and searchBar on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Body } from "./components/Body/Body.tsx";
+import { Header } from "./components/Header/Header.tsx";
+import { SearchBar } from "./components/SearchBar/SearchBar.tsx";
 import { MainRoutes } from "./routes/MainRoutes.tsx";
 
 function App() {
-  return <MainRoutes />;
+  return (
+    <Body>
+      <Header />
+      <SearchBar />
+      <MainRoutes />
+    </Body>
+  );
 }
 
 export default App;
